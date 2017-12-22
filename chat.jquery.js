673230ef74c630adfdeab3c890d7958ec905d967
Ramindu Deshapriya <rasade88@gmail.com>
2017-12-22 12:24:07 +0530

    Added send message

--- chat.jquery.js
+++ chat.jquery.js
@@ -14,4 +14,21 @@
 
     });
 
+    $(document).keypress(function(e) {
+        if(e.which === 13) {
+            var text = $('#chat-input').val();
+            $('#chat-input').val('');
+            var date = new Date();
+            var nowTime = date.getHours() + ':' + date.getMinutes();
+            $('.chat-history').append('<div class="chat-message clearfix">\n' +
+                '<img src="img/default-profile.png" alt="" width="32" height="32">' +
+                '                <div class="chat-message-content clearfix">\n' +
+                '                    <span class="chat-time">' + nowTime + '</span>\n' +
+                '                    <h5>Me</h5>\n' +
+                '                    <p>' + text + '</p>\n' +
+                '                </div> <!-- end chat-message-content -->\n' +
+                '            </div><hr>');
+        }
+    })
+
 }) ();
