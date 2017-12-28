9f657fb6511c8ac4be5518397fff6d7004ea2941
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2017-12-28 16:23:18 +0530

    Added auto-scroll to chat history

--- chat.jquery.js
+++ chat.jquery.js
@@ -32,21 +32,26 @@
                 '                    <p>' + text + '</p>\n' +
                 '                </div> <!-- end chat-message-content -->\n' +
                 '            </div><hr>');
+
+
+            $(".chat-history").stop().animate({ scrollTop: $(".chat-history")[0].scrollHeight }, 1000);
         }
     });
 
     function sendMessageToBot(message) {
-        $.get('http://localhost:3001/v1/bot/chat?text=' + message, function(res) {
+        $.get('http://localhost:3001/v1/bot/chat?text=' + message, function (res) {
             var date = new Date();
             var nowTime = date.getHours() + ':' + date.getMinutes();
             $('.chat-history').append('<div class="chat-message clearfix">\n' +
-            '<img src="img/bot-leopard.jpg" alt="" width="32" height="32">' +
-            '                <div class="chat-message-content clearfix">\n' +
-            '                    <span class="chat-time">' + nowTime + '</span>\n' +
-            '                    <h5>Bot</h5>\n' +
-            '                    <p>' + res.text + '</p>\n' +
-            '                </div> <!-- end chat-message-content -->\n' +
-            '            </div><hr>');
+                '<img src="img/bot-leopard.jpg" alt="" width="32" height="32">' +
+                '                <div class="chat-message-content clearfix">\n' +
+                '                    <span class="chat-time">' + nowTime + '</span>\n' +
+                '                    <h5>Bot</h5>\n' +
+                '                    <p>' + res.text + '</p>\n' +
+                '                </div> <!-- end chat-message-content -->\n' +
+                '            </div><hr>');
+
+            $(".chat-history").stop().animate({ scrollTop: $(".chat-history")[0].scrollHeight }, 1000);
         });
     }
 
